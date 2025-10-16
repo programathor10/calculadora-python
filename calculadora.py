b451bb6524517b204f2754b71aa08ea5957378fc
programathor10 <brunojobs@proton.me>
2025-10-16 10:19:20 -0300

    Refactor calculadora.py to modularize operations into separate functions and improve user interaction with a menu system. Added error handling for invalid inputs and division by zero.

--- calculadora.py
+++ calculadora.py
@@ -1,35 +1,76 @@
-n = 1
-def calculo(a,b,op):
-    if op == 1:
-        texto=(f'El resultado de la suma {a} + {b} es :{a + b} \n')
-    elif op == 2:
-        texto=(f'El resultado de la resta {a} - {b} es :{a - b} \n')
-    elif op == 3:
-        texto=(f'El resultado de la multiplicacion {a} * {b} es :{a * b} \n')
-    else:
-        texto=(f'El resultado de la division {a} / {b} es :{a / b} \n')
+def suma(a,b):
+    return a + b
     
-    return texto
 
+def resta(a,b):
+    return a - b
+     
 
-
-while n < 5 and n > 0 :
+def multiplicacion(a,b):
+    return a * b
     
-    print('1. para sumar')
-    print('2. para restar')
-    print('3. multiplicar')
-    print('4. dividir')
-    print('Cualquier numero fuera del rango de indice del menu terminara la ejecucion de este programa.')
-    
-    
-    n = int(input('selecciona su operacion: '))
-    if n < 5 and n > 0:
 
-        a = int(input('ingrese el primer valor: '))
-        b = int(input('ingrese el segundo valor: '))
-        print(calculo(a,b,n))
-        n = 1
-    else:
-        break
-print('programa finalizado.')
+def division(a,b):    
+    return a / b
+    
 
+
+def menu():
+    print("1. para sumar")
+    print("2. para restar")
+    print("3. multiplicar")
+    print("4. dividir")
+    print("0. Cerrar aplicacion")
+
+    n = int(input("selecciona su operacion: "))
+
+    return n
+
+
+def calcular(n):
+    
+    try:
+        a = float(input("ingrese el primer valor: "))
+        b = float(input("ingrese el segundo valor: "))
+        
+        if n == 1:
+            resultado = suma(a,b)
+        elif n == 2:
+            resultado = resta(a,b)
+        elif n == 3:
+            resultado = multiplicacion(a,b)
+        elif n == 4:
+            try:
+                resultado = division(a,b)
+            except ZeroDivisionError:
+                resultado = "El divisor no puede ser 0"
+        
+        return resultado
+    
+    except ValueError:
+        resultado = "Solo se opera con valores numericos."
+        return resultado
+
+
+
+def main():
+
+    n=-1
+
+    while n !=0:
+        try:
+            n=menu()
+            if n == 0:
+                break  
+        except ValueError:
+            print("ingrese un valor numerico.")
+            continue
+        
+        if n not in range(0, 5):
+            print("Ingresá un número entre 0 y 4")
+            continue
+        resultado = calcular(n)
+        print(resultado)
+
+
+main()
